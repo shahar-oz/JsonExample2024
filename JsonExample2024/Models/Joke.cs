e3310052c952fe28145e082122befede3dfe3650
User <User@LAB3-3>
2024-03-27 09:34:20 +0200

    gt

---
+++ JsonExample2024/Models/Joke.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonExample2024.Models
+{
+    internal class Joke
+    
+        {
+    "error": false,
+    "category": "Misc",
+    "type": "twopart",
+    "setup": "I like my girls how I like my COVID.",
+    "delivery": "19 and easily spread.",
+    "flags": {
+        "nsfw": true,
+        "religious": false,
+        "political": false,
+        "racist": false,
+        "sexist": false,
+        "explicit": true
+    },
+    "id": 167,
+    "safe": false,
+    "lang": "en"
+}
+}
+
